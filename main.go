d244d472d471815dca1e8c193ffa7a38a05d913a
Joel Howse <joelhowse@gmail.com>
2021-09-23 00:07:37 +1200

    🎨 better logging

--- main.go
+++ main.go
@@ -61,28 +61,27 @@ func (t DurationTicks) Ticks(min, max float64) []plot.Tick {
 func main() {
 
 	var CPU = cpuid.CPU
-	// Print basic CPU information:
+	fmt.Println("CPU Information (similar to lscpu)")
 	fmt.Println("Cache Line Size:", CPU.CacheLine, "bytes")
 	fmt.Println("L2 Cache:", formatBytes(int64(CPU.Cache.L2)))
 	fmt.Println("L3 Cache:", formatBytes(int64(CPU.Cache.L3)))
 
-	// cache_lines()
+	fmt.Println("Running estimation algorithm")
 	estimate := estimate_llc_size()
 	fmt.Println("Estimated LLC size:", formatBytes(int64(estimate)))
 }
 
 func estimate_llc_size() int {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
 	// Use a random source so we index the array randomly.
 	// This should help mess with the prefetching.
+	// It should also handle the issues with cachelines.
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	testRange := func(sizes []int) plotter.XYs {
 		const iterations = 1024 * 1024 * 16
 
 		xys := make(plotter.XYs, 0, len(sizes))
 		for _, size := range sizes {
-			fmt.Println("Size:", formatBytes(int64(size)))
-
 			// an int32 is 4 bytes, so we allocate size / 4
 			arr := make([]int32, size/4)
 			arrLength := len(arr)
@@ -92,6 +91,7 @@ func estimate_llc_size() int {
 				arr[r.Int()%arrLength]++
 			}
 			elapsed := time.Since(start)
+			fmt.Printf("Array size: %-16s Duration: %s\n", formatBytes(int64(size)), elapsed)
 
 			xys = append(xys, plotter.XY{
 				X: float64(size),
@@ -160,41 +160,4 @@ func estimate_llc_size() int {
 	plotXYs("second", xys, convergedSizes)
 
 	return convergedSizes[maxSlope(xys)+1]
-}
-
-func cache_lines() {
-	const l = 1024 * 1024 * 32
-
-	arr := make([]int32, l)
-
-	xys := make(plotter.XYs, 128)
-	for k := 1; k <= 128; k++ {
-		// reset
-		for i := 0; i < l; i++ {
-			arr[i] = 4
-		}
-
-		start := time.Now()
-		for i := 0; i < l; i += k {
-			arr[i] *= 3
-		}
-		elapsed := time.Since(start)
-		fmt.Println(k, ":", elapsed)
-		xys[k-1].X = float64(k)
-		xys[k-1].Y = float64(elapsed.Nanoseconds())
-	}
-
-	p := plot.New()
-	p.Title.Text = "Cache lines measurement"
-	p.X.Label.Text = "Difference (K)"
-	p.Y.Label.Text = "Time (ns)"
-
-	p.Add(&plotter.Line{
-		XYs:       xys,
-		LineStyle: plotter.DefaultLineStyle,
-	})
-
-	if err := p.Save(20*vg.Centimeter, 20*vg.Centimeter, "caches.png"); err != nil {
-		panic(err)
-	}
 }
